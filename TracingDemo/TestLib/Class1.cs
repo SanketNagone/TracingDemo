a2d9193c56516221546ba1774f18b7f97cb327c4
Sanket Nagone <sanket.nagone@elliemae.com>
2018-03-13 14:58:51 +0530

    Added Test Lib project

---
+++ TracingDemo/TestLib/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    /// <summary>
+    /// 
+    /// Test Project class 
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// Test Sub method
+        /// </summary>
+        /// <param name="a">First Number</param>
+        /// <param name="b">Second Number</param>
+        /// <returns></returns>
+        public int sub(int a, int b)
+        {
+            return 2;
+        }
+
+        /// <summary>
+        /// Random Number generator
+        /// </summary>
+        /// <param name="max">Max number to be generated</param>
+        /// <returns></returns>
+        public float RandimNum(int max)
+        {
+            return 1.1f;
+        }
+    }
+}
---
+++ TracingDemo/TracingDemo/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TracingDemo
+{
+    /// <summary>
+    /// Test Documentation class
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Test Add Method
+        /// </summary>
+        /// <param name="a">First Param</param>
+        /// <param name="b">Second Param</param>
+        /// <returns></returns>
+        public static int ADD(int a, int b)
+        {
+            return 2;
+        }
+
+        /// <summary>
+        /// Test ReturnName 
+        /// </summary>
+        /// <param name="name">My name to be returned </param>
+        /// <returns></returns>
+        public static string MayName(string name)
+        {
+            return name;
+        }
+    }
+}
